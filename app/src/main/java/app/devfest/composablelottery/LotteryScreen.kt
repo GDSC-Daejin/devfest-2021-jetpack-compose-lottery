1120d6fcddf2c63df2ca98c30ba7cf6da8686455
HarryPark <jungsanmango@gmail.com>
2021-11-03 20:21:19 +0900

    프로젝트 정리

--- app/src/main/java/app/devfest/composablelottery/LotteryScreen.kt
+++ app/src/main/java/app/devfest/composablelottery/LotteryScreen.kt
@@ -1,10 +1,11 @@
 package app.devfest.composablelottery
 
 import androidx.compose.runtime.Composable
+import app.devfest.composablelottery.ui.SuperFancyButton
 
 @Composable
 fun LotteryScreen() {
 
-
+    SuperFancyButton()
 
 }
