3be6c72b5f5c30a247492f09644dc86e3c67767f
soopeach <hsjeon584@gmail.com>
2023-04-30 17:51:34 +0900

    LotteryPickButton 구현

--- app/src/main/java/app/devfest/composablelottery/ui/LotteryPickButton.kt
+++ app/src/main/java/app/devfest/composablelottery/ui/LotteryPickButton.kt
@@ -1,12 +1,14 @@
 package app.devfest.composablelottery.ui
 
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun LotteryPickButton(
-    totalLottoList : List<Int>,
-    pickedLottoList : List<Int>,
-    onLottoNumberPicked : (Int) -> Unit,
+    totalLottoList: List<Int>,
+    pickedLottoList: List<Int>,
+    onLottoNumberPicked: (Int) -> Unit,
     onResetLottoList: () -> Unit
 ) {
 
@@ -19,8 +21,16 @@ fun LotteryPickButton(
      * */
     if (pickedLottoList.size == 6) {
         // 다시 뽑기 버튼 구현
+        Button(onClick = { onResetLottoList() }) {
+            Text(text = "다시 뽑기")
+        }
     } else {
         // 뽑기 버튼 구현
+        Button(onClick = {
+            onLottoNumberPicked((totalLottoList - pickedLottoList.toSet()).shuffled().first())
+        }) {
+            Text(text = "뽑기")
+        }
     }
 
 }
