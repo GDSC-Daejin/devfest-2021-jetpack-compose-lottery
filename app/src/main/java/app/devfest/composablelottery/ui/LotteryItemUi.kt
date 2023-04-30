46847da692f33b750aa394959a24749f1ec70f3e
soopeach <hsjeon584@gmail.com>
2023-04-30 17:31:53 +0900

    LotteryItemUi 구현

--- app/src/main/java/app/devfest/composablelottery/ui/LotteryItemUi.kt
+++ app/src/main/java/app/devfest/composablelottery/ui/LotteryItemUi.kt
@@ -1,9 +1,19 @@
 package app.devfest.composablelottery.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import app.devfest.composablelottery.AppTheme
 
 @Composable
@@ -21,6 +31,21 @@ fun LotteryItemUi(lottoNumber: Int) {
         in 31 .. 40 -> Color(0xFF505050)
         in 31 .. 45 -> Color(0xFF46A22C)
         else -> Color(0x00000000)
+    }
+
+    Box(modifier = Modifier
+        .clip(CircleShape)
+        .background(ballColor)
+        .size(50.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(modifier = Modifier
+            .clip(CircleShape)
+            .background(Color.White)
+            .size(25.dp),
+            contentAlignment = Alignment.Center) {
+            Text(text = lottoNumber.toString(), color = Color.Black, fontWeight = FontWeight.Bold)
+        }
     }
 
 }
